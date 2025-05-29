5ac812914626a86c2eb6b9aa0b223356ead4bf01
Nohaa4 <nohafauziah@gmail.com>
2025-05-29 16:41:36 +0700

    first commit

---
+++ app/Views/percabangan.php
@@ -0,0 +1,22 @@
+<?php
+$nilai = 'B';
+if ($nilai == 'A') {
+    echo "Sangat Baik";
+}
+else if ($nilai == 'B') {
+    echo "Baik";
+}
+else if ($nilai == 'C') {
+
+    echo "Cukup";
+}
+else if ($nilai == 'D') {
+    echo "Kurang";
+} else if ($nilai == 'E') 
+    echo "Sangat Kurang";
+
+else {
+    echo  "Silahkan input nilai dengan baik ";
+        
+    
+}
